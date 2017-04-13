dd94b0ba93e11fdd87029519d3684bd6394eb44e
Faim Khandker <faim.khandker@gmail.com>
2017-04-12 22:11:51 -0400

    Commit 6
    Added item pick up script, but needs more tuning and added selina's update PlayerController script tweaked out mouse control

---
+++ Assets/_Scripts/ItemPickUpController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickUpController : MonoBehaviour 
+{
+	//Public Instance Variables
+	public int distanceToitem;
+	public GameObject item;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Collect();
+	}
+
+	//Methods
+	void Collect()
+	{
+//		if (Input.GetMouseButtonUp (1)) 
+//		{
+//			RaycastHit hit; //
+//			//
+//			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+//
+//			//Nested - 
+//			if (Physics.Raycast (ray, out hit, distanceToitem)) 
+//			{
+//				//Nested nested - 
+//				if(hit.collider.gameObject == "modelBonfire")
+//				{
+//					//Debugging
+//					Debug.Log("Item has been hit");
+//					Destroy (hit.collider.gameObject); //destroys the "item"
+//				}
+//			}
+//
+//		}
+	}
+}
